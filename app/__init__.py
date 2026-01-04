6ce2bc2e78f7991b61517093d1394dfa284ef644
cyb3erasad <cyb3rasad@gmail.com>
2026-01-04 22:45:07 +0500

    Add base of project

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extension import db, login_manager
+from .extension import db, login_manager, migrate
 
 def create_app():
     app = Flask(__name__)
@@ -7,5 +7,20 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)
+    
+
+    from auth import auth_bp
+    from books import books_bp
+    from cart import cart_bp
+    from orders import orders_bp
+    from admin import admin_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(books_bp)
+    app.register_blueprint(cart_bp)
+    app.register_blueprint(orders_bp)
+    app.register_blueprint(admin_bp)
+
 
     return app
